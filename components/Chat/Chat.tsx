2783dd91cdf549ee46c5845e88e063d13503e8cc
Christian Landgren <christian@landgren.nu>
2023-12-17 22:26:35 +0100

    chore: more translations

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -29,10 +29,10 @@ import Spinner from '../Spinner';
 import { ChatInput } from './ChatInput';
 import { ChatLoader } from './ChatLoader';
 import { ErrorMessageDiv } from './ErrorMessageDiv';
+import { MemoizedChatMessage } from './MemoizedChatMessage';
 import { ModelSelect } from './ModelSelect';
 import { SystemPrompt } from './SystemPrompt';
 import { TemperatureSlider } from './Temperature';
-import { MemoizedChatMessage } from './MemoizedChatMessage';
 
 interface Props {
   stopConversationRef: MutableRefObject<boolean>;
@@ -352,18 +352,19 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
       {!(apiKey || serverSideApiKeyIsSet) ? (
         <div className="mx-auto flex h-full w-[300px] flex-col justify-center space-y-6 sm:w-[600px]">
           <div className="text-center text-4xl font-bold text-black dark:text-white">
-            Welcome to Chatbot UI
+            Welcome to Mistral UI
           </div>
           <div className="text-center text-lg text-black dark:text-white">
             <div className="mb-8">{`Chatbot UI is an open source clone of OpenAI's ChatGPT UI.`}</div>
             <div className="mb-2 font-bold">
-              Important: Chatbot UI is 100% unaffiliated with OpenAI.
+              Important: Mistral UI is 100% unaffiliated with OpenAI.
             </div>
           </div>
           <div className="text-center text-gray-500 dark:text-gray-400">
             <div className="mb-2">
-              Chatbot UI allows you to plug in your API key to use this UI with
-              their API.
+              Mistral UI hjälper dig att utvärdera den europeiska AI tjänsten
+              Mistral som ännu inte har ett UI men har ett bra API som du kan
+              testa genom att anävnda denna tjänst.
             </div>
             <div className="mb-2">
               It is <span className="italic">only</span> used to communicate
@@ -405,7 +406,7 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
                         <Spinner size="16px" className="mx-auto" />
                       </div>
                     ) : (
-                      'Chatbot UI'
+                      'Mistral UI'
                     )}
                   </div>
 
--- components/Chat/ChatLoader.tsx
+++ components/Chat/ChatLoader.tsx
@@ -1,7 +1,9 @@
 import { IconRobot } from '@tabler/icons-react';
 import { FC } from 'react';
 
-interface Props { }
+import Image from 'next/image';
+
+interface Props {}
 
 export const ChatLoader: FC<Props> = () => {
   return (
@@ -11,7 +13,7 @@ export const ChatLoader: FC<Props> = () => {
     >
       <div className="m-auto flex gap-4 p-4 text-base md:max-w-2xl md:gap-6 md:py-6 lg:max-w-2xl lg:px-0 xl:max-w-3xl">
         <div className="min-w-[40px] items-end">
-          <IconRobot size={30} />
+          <Image src="/favicon.ico" alt="M" />
         </div>
         <span className="animate-pulse cursor-default mt-1">▍</span>
       </div>
